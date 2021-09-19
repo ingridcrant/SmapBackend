a7e56459e9e93f3d65c2e02188e17a96e0b5c838
ingridcrant <ingridcrant@gmail.com>
2021-09-19 03:05:59 -0400

    gunicorn line fixed

--- app/test.py
+++ app/test.py
@@ -1,16 +1,16 @@
 import requests
 
-rv = requests.post('http://s-map.herokuapp.com/postbuildinginfo', json={
+rv = requests.post('https://s-map.herokuapp.com/postbuildinginfo', json={
     "buildingname": "MC",
     "wifi": True,
     "ports": False,
     "soundlevel": 2
 })
 print(rv)
-rv2 = requests.post('http://s-map.herokuapp.com/updatebuildinginfo', json={
+rv2 = requests.post('https://s-map.herokuapp.com/updatebuildinginfo', json={
     "buildingname": "MC",
     "name": "First Last"
 })
-
-rv3 = requests.get('http://s-map.herokuapp.com/getbuildinginfo')
+print(rv2)
+rv3 = requests.get('https://s-map.herokuapp.com/getbuildinginfo')
 print(rv3.json())
