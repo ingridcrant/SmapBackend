79489591de04724337a47f62ce99c3b7c7cabb5e
ingridcrant <ingridcrant@gmail.com>
2021-09-19 03:17:39 -0400

    working backend

--- app/test.py
+++ app/test.py
@@ -1,15 +1,15 @@
 import requests
 
 rv = requests.post('https://s-map.herokuapp.com/postbuildinginfo', json={
-    "buildingname": "MC",
-    "wifi": True,
-    "ports": False,
-    "soundlevel": 2
+    'buildingname': 'MC',
+    'wifi': True,
+    'ports': False,
+    'soundlevel': 2
 })
 print(rv)
 rv2 = requests.post('https://s-map.herokuapp.com/updatebuildinginfo', json={
-    "buildingname": "MC",
-    "name": "First Last"
+    'buildingname': 'MC',
+    'name': 'Hey Last'
 })
 print(rv2)
 rv3 = requests.get('https://s-map.herokuapp.com/getbuildinginfo')
